c094767c03b4c008ce63d1f512672a416e384011
Andrew Jones <andrew@bidops.com>
2019-04-22 11:28:34 -0700

    pubsub install and flipcoin mutation

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const { graphqlExpress, graphiqlExpress, ApolloServer, gql } = require('apollo-server-express')
 const { makeExecutableSchema } = require('graphql-tools')
 const { execute, subscribe } = require('graphql')
 const { createServer } = require('http')
+const { PostgresPubSub } = require("graphql-postgres-subscriptions");
+
 
 const database = require('./database')
+
+const pubsub = new PostgresPubSub({
+  user: process.env.USER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+})
 
 const PORT = process.env.PORT || 3001
 const HOST = process.env.HOST || 'localhost'
@@ -13,6 +25,8 @@ const HOST = process.env.HOST || 'localhost'
 const typeDefs = gql`
   type Coinflip { id: Int!, isFlipped: Boolean!, heads: Boolean }
   type Query { newCoinflip: Coinflip }
+  type Mutation { flipCoin(id: Int!): Boolean }
+  type Subscription { CoinflipGame(id: Int!): Coinflip }
 `
 
 const resolvers = {
@@ -20,8 +34,17 @@ const resolvers = {
     newCoinflip: async () => {
       const result = await database('coinflip').insert({ isFlipped: false }).returning(['id', 'isFlipped'])
       const { id, isFlipped } = result[0] ;
+      console.log(process.env.PGDATABASE)
       return { id, isFlipped} ;
     },
+  },
+  Mutation: {
+    flipCoin: async (id) => {
+      const heads = Math.random() >= 0.5;
+      const [result] = await database('coinflip').where({id}).returning("heads").insert({ heads });
+      console.log(result);
+      return result;
+    }
   }
 }
 
