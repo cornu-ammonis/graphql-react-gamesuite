008af7b4943eed6c8006f61a0d2a2595a4c674f0
Andrew Jones <andrew@bidops.com>
2019-04-23 11:59:57 -0700

    working apollo query

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Coin from './Coin';
+import gql from "graphql-tag";
+
+import { ApolloProvider, Query, Mutation } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: "http://localhost:3001/graphql",
+      credentials: "same-origin"
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+const GET_COINFLIP_QUERY = gql`
+  {
+    newCoinflipSession {
+      id
+    }
+  }
+  `;
+
+const GetCoinflipQuery = ({children}) => (
+  <Query query={GET_COINFLIP_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading)
+        return (
+          <div style={{ paddingTop: 20 }}>
+            <div>Loading...</div>
+          </div>
+        );
+      if (error) return <p>Error :(</p>;
+      
+        console.log(data)
+    
+      return children(data.id)
+    }}
+  </Query>
+);
 
 class App extends Component {
   
@@ -10,28 +63,41 @@ class App extends Component {
 
     if (route === 'coin')
       return (
-        <div className="App">
-          <Coin/>
-        </div>
+        <ApolloProvider client={client}>
+          <div className="App">
+            <Coin/>
+          </div>
+        </ApolloProvider>
       );
 
+    if (route === 'newCoin') {
+     return( 
+      <ApolloProvider client={client}>
+        <GetCoinflipQuery>{id => <Coin id={id}/>}</GetCoinflipQuery>
+      </ApolloProvider>
+     );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <a href='/newCoin'> New Coinflip Game </a>
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- client/src/Coin.jsx
+++ client/src/Coin.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import gql from "graphql-tag";
+import { ApolloProvider, Query, Mutation } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+
 
 const Coin = props => {
   return (
